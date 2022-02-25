09f61ccc5cc593b7b192cfb184a8901f33e2db7e
Riuminjake <64644764+Riuminjake@users.noreply.github.com>
2022-02-25 23:38:45 +0300

    referencii

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,21 +1,30 @@
 package sk.stuba.fei.uim.oop;
 
-import java.util.Objects;
-import java.util.Scanner;
+
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-        System.out.println("zadai");
-        String c=scan.nextLine();
-        System.out.println(c);
-        String a="a";
-        String b="a";
-        System.out.println("a==b:"+(a.equals(b)));
-        System.out.println("a==c:"+(a.equals(c)));
+        int x=6;
+        int[] pole=new int[]{1,2,3,4};
+        System.out.println(x);
+        testP(x);
+        System.out.println(Arrays.toString(pole));
+        testPole(pole);
+        System.out.println(Arrays.toString(pole));
+        System.out.println(x);
+    }
+    public static void testP(int p){
+        p=10;
+        System.out.println(p);
+    }
 
-        System.out.println("objectsequals"+ Objects.equals(a,c));
-
+    public static void testPole(int[] array){
+        array[0]=10;
+        System.out.println(Arrays.toString(array));
 
     }
+
+
 }
