8133c795239c62023f02b9fc4b2972b56097816e
Riuminjake <64644764+Riuminjake@users.noreply.github.com>
2022-02-25 23:17:12 +0300

    porovanie

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,21 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("zadai");
+        String c=scan.nextLine();
+        System.out.println(c);
+        String a="a";
+        String b="a";
+        System.out.println("a==b:"+(a.equals(b)));
+        System.out.println("a==c:"+(a.equals(c)));
+
+        System.out.println("objectsequals"+ Objects.equals(a,c));
+
 
     }
 }
